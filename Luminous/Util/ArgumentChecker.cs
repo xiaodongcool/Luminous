e7d5e592507898d05e7efaa34da8362be8ae3791
Admin <Admin@LAPTOP-EPRK6HUC>
2023-08-19 21:50:31 +0800

    1

--- Luminous/Util/ArgumentChecker.cs
+++ Luminous/Util/ArgumentChecker.cs
@@ -18,4 +18,22 @@
             }
         }
     }
+
+    public static class ArgumentGuard
+    {
+        public static void CheckForNull(string argument, string argumentName)
+        {
+            if (string.IsNullOrEmpty(argument))
+            {
+                throw new ArgumentNullException(argumentName, $"{argumentName} 不能为空");
+            }
+        }
+        public static void CheckForNull(object argument, string argumentName)
+        {
+            if (argument == null)
+            {
+                throw new ArgumentNullException(argumentName, $"{argumentName} 不能为 null");
+            }
+        }
+    }
 }
